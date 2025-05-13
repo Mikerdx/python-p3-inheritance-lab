8aad0854a8df8212525d876a24eae1c969128ac5
Mike Bett <mike.bett@student.moringa.school.com>
2025-05-13 12:05:38 +0300

    done

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,11 @@ from user import User
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.knowledge = [","]
 
     def teach(self):
+        return self.knowledge[0]
         pass
